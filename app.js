f0fa20ede0ecfaa0844fe3f671d4344ed71cbb82
jaeyeon kim <foodsksms@gmail.com>
2020-02-06 14:44:07 +0900

    [issue-1] lint검사
    - npm run lint 명령어 추가.
    - 간단한 오류는 --fix옵션으로 수정해준다.

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ import {chromium} from 'playwright';
 
 const openBrowser = async () => {
   const browser = await chromium.launch();
-  console.log(`b : ${browser}`);
   const context = await browser.newContext();
   const page = await context.newPage('https://www.google.com/');
 
-  await page.screenshot({ path: `2.png` });
+  await page.screenshot({path: `2.png`});
   await browser.close();
 };
 
