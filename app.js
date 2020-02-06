fce1acc9da53ff5313f7d8fb8a267cb607eb01e5
jaeyeon kim <foodsksms@gmail.com>
2020-02-06 14:39:54 +0900

    [issue-1] 페이지 띄어보기
    - package.json에 babel-node 명령어를 이용해서 es6문법으로 실행되도록 설정
    - document에 나와있는 screenshop을 테스트

---
+++ app.js
@@ -0,0 +1,13 @@
+import {chromium} from 'playwright';
+
+const openBrowser = async () => {
+  const browser = await chromium.launch();
+  console.log(`b : ${browser}`);
+  const context = await browser.newContext();
+  const page = await context.newPage('https://www.google.com/');
+
+  await page.screenshot({ path: `2.png` });
+  await browser.close();
+};
+
+openBrowser();
