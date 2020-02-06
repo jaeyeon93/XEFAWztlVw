842aea1ad5d5afd606370c02ccea33bdb4bb8ef7
jaeyeon kim <foodsksms@gmail.com>
2020-02-06 16:37:57 +0900

    [issue-1] 로그인 성공
    - login메서드 구현
    - login메서드에서 사용자에게 미리 입력받은 id, pw를 인자로 받는다. github.com에 들어가면 sign up을 하기 위해서 버튼들을 클릭하고 인자로 전달받은 id, pw를 입력해서 로그인을 한다.

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 import {chromium} from 'playwright';
 
 const openBrowser = async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({
+    headless: false
+  });
   const context = await browser.newContext();
   const page = await context.newPage('https://www.github.com/');
-  await login(page);
+  await login(page, 'id', 'pw@');
   console.log(`login function closed`);
-  // await page.screenshot({path: `screenshot.png`});
+  await page.screenshot({path: `screenshot.png`});
   await browser.close();
 };
 
-const login = async (page) => {
+const login = async (page ,id, pw) => {
   try {
+    await page.click('body > div.position-relative.js-header-wrapper > header > div > div.d-flex.flex-justify-between.flex-items-center > div.d-flex.flex-items-center > button');
     await page.click('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3');
-    await page.screenshot({path: `loginbutton.png`});
-    return 0;
+    await page.evaluate((id, pw) => {
+      document.querySelector('#login_field').value = id;
+      document.querySelector('#password').value = pw;
+    }, id, pw).then(() => page.click('#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block'));
+    await page.screenshot({path: `login.png`});
+    return page;
   } catch (error) {
     console.log(error);
   }
-
 }
 
 openBrowser();
