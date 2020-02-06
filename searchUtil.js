02fd5bd21ec2e1d3251e93abd2d4ad6ab904cdbf
jaeyeon kim <foodsksms@gmail.com>
2020-02-06 19:40:02 +0900

    [issue-2] issue2 중간커밋
    - searchUtil.js에 대한 부분을 구현중, 중간커밋

--- searchUtil.js
+++ searchUtil.js
@@ -1,10 +1,10 @@
 const searchRepo = async (page, repoName) => {
-  console.log(`search Repo called, repo name is ${repoName}`);
+  page.screenshot({path:'search.png'});
   try {
     await page.evaluate((repoName) => {
       console.log(`전달받은 repo ${repoName}`);
       document.querySelector('  await login.login(page, `${process.env.ID}`, `${process.env.PW}`);\n').value = repoName;
-    }, repoName)
+    }, repoName).then(() => console.log(`search 성공`));
   } catch (err) {
     console.log(err);
   }
