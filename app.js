0036a1a6be85559db9fd91cec1c4bedaecfdb743
jaeyeon kim <foodsksms@gmail.com>
2020-02-06 15:18:09 +0900

    [issue-1] 로그인 만드는중
    - chromium으로 github.com으로 접속을 하면 새로 로그인을 해야한다. signup button을 찾아서 click하는 부분을 구현중

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ import {chromium} from 'playwright';
 const openBrowser = async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext();
-  const page = await context.newPage('https://www.google.com/');
-
-  await page.screenshot({path: `2.png`});
+  const page = await context.newPage('https://www.github.com/');
+  await login(page);
+  console.log(`login function closed`);
+  // await page.screenshot({path: `screenshot.png`});
   await browser.close();
 };
+
+const login = async (page) => {
+  try {
+    await page.click('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3');
+    await page.screenshot({path: `loginbutton.png`});
+    return 0;
+  } catch (error) {
+    console.log(error);
+  }
+
+}
 
 openBrowser();
