ab606bde73d9d04c5ec670367e76d7874ce578fe
JaeyeonKIM <foodsksms@gmail.com>
2020-02-08 18:29:08 +0900

    [issue-3] 이슈등록 성공
    - 기존에 page.evaluate를 입력하던것을 page.type으로 변경

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ import {chromium} from 'playwright';
 import dotenv from 'dotenv';
 import loginUtil from './loginUtil';
 import searchUtil from './searchUtil';
+import addUtil from './addUtil';
 
 dotenv.config();
 
 const app = async () => {
   const page = await openBrowser();
   const loginResult = await loginUtil.login(page, `${process.env.ID}`, `${process.env.PW}`);
-  await searchUtil.searchRepo(loginResult, `${process.env.REPO}`);
+  const helpmeRepoPage = await searchUtil.searchRepo(loginResult, `${process.env.REPO}`);
+  // await addUtil.addNewIssue(await addUtil.findIssueTab(helpmeRepoPage), 'jimmy test', 'hello world');
+  await addUtil.addNewIssue(await addUtil.findIssueTab(helpmeRepoPage), '이슈 테스트중', 'comment에 대한 부분 테스트');
 };
 const openBrowser = async () => {
   const browser = await chromium.launch({
