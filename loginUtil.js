84b11e9b477897fd4fbf0e4ac99dc69cf2a26463
jaeyeon kim <foodsksms@gmail.com>
2020-02-08 17:35:59 +0900

    [issue-3] issue3시작
    - issue3시작. loginUtil.js에서 selector를 이용한 검색수정

--- loginUtil.js
+++ loginUtil.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 const loginUtil = async (page, id, pw) => {
   try {
-    await page.click('body > div.position-relative.js-header-wrapper > header > div > div.d-flex.flex-justify-between.flex-items-center > div.d-flex.flex-items-center > button');
-    await page.click('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3');
+    await page.click('header div.d-flex.flex-justify-between.flex-items-center > div.d-flex.flex-items-center > button');
+    await page.click('header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3');
     await page.evaluate((id, pw) => {
       document.querySelector('#login_field').value = id;
       document.querySelector('#password').value = pw;
