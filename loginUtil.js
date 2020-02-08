98dc5aea9eaae9c55d68dcb0f9966337e1ea84fb
JaeyeonKIM <foodsksms@gmail.com>
2020-02-08 20:31:02 +0900

    [issue-4] login에 대한 테스트코드
    - mocha, chai를 이용한 login에 대한 테스트 코드 작성중.
    - chai, mocah, babel-register, babel-polyfill 의존성 추가
    - npm run test명령어에서 es6가 동작할 수 있도록 구성. timeout시간추가

--- loginUtil.js
+++ loginUtil.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const loginUtil = async (page, id, pw) => {
+const login = async (page, id, pw) => {
   try {
     await page.click('header div.d-flex.flex-justify-between.flex-items-center > div.d-flex.flex-items-center > button');
     await page.click('header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > a.HeaderMenu-link.no-underline.mr-3');
@@ -13,7 +13,7 @@ const loginUtil = async (page, id, pw) => {
     if (loginValid) {
       return page;
     } else {
-      console.log(`로그인실패`);
+      // throw new Error('로그인에 실패했습니다');
       return page.goto(loginUrl);
     }
   } catch (error) {
@@ -29,5 +29,5 @@ const checkLoginValid = async (url) => {
 };
 
 module.exports = {
-  login: loginUtil,
+  login,
 };
