8e3abc268b8ee47c8783557a458ee831437a6a8d
JaeyeonKIM <foodsksms@gmail.com>
2020-02-11 23:43:05 +0900

    [issue-7] 마무리
    - 기타마무리작업

--- app.js
+++ app.js
@@ -13,20 +13,17 @@ const app = async () => {
     const loginResult = await loginUtil.login(page, `${process.env.ID}`, `${process.env.PW}`);
     const helpmeRepoPage = await searchUtil.searchRepo(loginResult, `${process.env.REPO}`);
     const issuePage = await addUtil.addIssueOnRepo(helpmeRepoPage, '백엔드 엔지니어 2차면접 과제[김재연]');
-    await issuePage.wait(1000);
     await issuePage.close();
   } catch (error) {
     await logging.captureError(error);
   } finally {
-    // await page.close();
+    await page.close().then(() => console.log('crawling is finished'));
   }
 };
 
 const openBrowser = async () => {
   try {
-    const browser = await chromium.launch({
-      headless: false,
-    });
+    const browser = await chromium.launch();
     const context = await browser.defaultContext();
     const page = await context.newPage(`${process.env.GITHUB}`);
     return page;
