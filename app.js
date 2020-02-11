bf20a3f201bcb76317c9b1cd4610780bf988e800
JaeyeonKIM <foodsksms@gmail.com>
2020-02-11 23:27:52 +0900

    [issue-7] login 메서드 수정
    - 네트워크 속도에 따라 지연되서 안되는 부분이 있어서 page.waitForSelector()로 변경

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const app = async () => {
   try {
     const loginResult = await loginUtil.login(page, `${process.env.ID}`, `${process.env.PW}`);
     const helpmeRepoPage = await searchUtil.searchRepo(loginResult, `${process.env.REPO}`);
-    const issuePage = await addUtil.addIssueOnRepo(helpmeRepoPage, 'chromium');
-    await issuePage.wait(1000);
-    await issuePage.close();
+    // const issuePage = await addUtil.addIssueOnRepo(helpmeRepoPage, '백엔드 엔지니어 2차면접 과제[김재연]');
+    // await issuePage.wait(1000);
+    // await issuePage.close();
   } catch (error) {
     await logging.captureError(error);
   } finally {
-    await page.close();
+    // await page.close();
   }
 };
 
